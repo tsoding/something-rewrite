c6efde0b7074321729024e25c625ded147ff3a5f
Danil Kolumbet <kolumbetko@gmail.com>
2021-03-21 21:27:11 +0200

    (#18) Add CI for Windows

--- setup_dependencies.bat
+++ setup_dependencies.bat
@@ -2,6 +2,7 @@
 
 curl -fsSL -o SDL2-devel-2.0.12-VC.zip https://www.libsdl.org/release/SDL2-devel-2.0.12-VC.zip
 tar -xf SDL2-devel-2.0.12-VC.zip
+if not exist dependencies\ mkdir dependencies\
 move SDL2-2.0.12 dependencies\SDL2
 del SDL2-devel-2.0.12-VC.zip
 if not exist dependencies\SDL2\temp\ mkdir dependencies\SDL2\temp\
