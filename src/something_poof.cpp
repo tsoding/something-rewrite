fee79852b73b501bc314fe886a194b539d5dfb89
rexim <reximkut@gmail.com>
2021-03-22 19:11:42 +0700

    Introduce Poof entity for testing split triangles

---
+++ src/something_poof.cpp
@@ -0,0 +1,56 @@
+#include "./something_poof.hpp"
+
+float random01()
+{
+    static_assert(RAND_MAX != 0);
+    return static_cast<float>(rand()) / static_cast<float>(RAND_MAX);
+}
+
+V2<float> point_on_triangle(Triangle<float> triangle,
+                            float f0, float f1)
+{
+    const auto a = triangle.vs[0];
+    const auto b = triangle.vs[1];
+    const auto c = triangle.vs[2];
+    return lerp(a, lerp(b, c, f0), f1);
+}
+
+void Poof::push(Triangle<float> triangle,
+                RGBA shade,
+                Triangle<float> uv,
+                int atlas_index)
+{
+    assert(poof_size < CAPACITY);
+
+    triangles[poof_size] = triangle;
+    shades[poof_size]    = shade;
+    uvs[poof_size]       = uv;
+    textures[poof_size]  = atlas_index;
+
+    positions[poof_size] = V2(0.0f);
+    velocities[poof_size] = V2<float>::polar(random01() * 2.0f * M_PI, random01() * MAX_VELOCITY);
+
+    angles[poof_size] = 0.0f;
+    angle_velocities[poof_size] = random01() * MAX_ANGLE_VELOCITY;
+    pivots[poof_size] = point_on_triangle(triangle, random01(), random01());
+
+    poof_size += 1;
+}
+
+void Poof::clear()
+{
+    poof_size = 0;
+}
+
+void Poof::update(Seconds dt)
+{
+    for (size_t i = 0; i < poof_size; ++i) {
+        positions[i] += velocities[i] * dt;
+        angles[i] += angle_velocities[i] * dt;
+    }
+}
+
+void Poof::render(Renderer *renderer) const
+{
+    assert(false && "Poof::render: not implemented");
+}
---
+++ src/something_poof.hpp
@@ -0,0 +1,32 @@
+#ifndef SOMETHING_POOF_HPP_
+#define SOMETHING_POOF_HPP_
+
+struct Poof {
+    static constexpr size_t CAPACITY = 69;
+    static constexpr float MAX_VELOCITY = 200.0f;
+    static constexpr float MAX_ANGLE_VELOCITY = M_PI;
+
+    Triangle<float>  triangles[CAPACITY];
+    RGBA             shades[CAPACITY];
+    Triangle<float>  uvs[CAPACITY];
+    int              textures[CAPACITY];
+
+    V2<float>        positions[CAPACITY];
+    V2<float>        velocities[CAPACITY];
+
+    float            angles[CAPACITY];
+    float            angle_velocities[CAPACITY];
+    V2<float>        pivots[CAPACITY];
+
+    size_t poof_size;
+
+    void push(Triangle<float> triangle,
+              RGBA shade,
+              Triangle<float> uv,
+              int atlas_index);
+    void clear();
+    void update(Seconds dt);
+    void render(Renderer *renderer) const;
+};
+
+#endif // SOMETHING_POOF_HPP_
