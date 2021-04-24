62df9bea1df40918e0bc21408b5e896de4e5afd3
rexim <reximkut@gmail.com>
2021-04-24 20:03:49 +0700

    Introduce Circle_Renderer

--- src/something.cpp
+++ src/something.cpp
@@ -26,6 +26,7 @@ const int SCREEN_FPS = 60;
 #include "./something_game.cpp"
 #include "./something_player.cpp"
 #include "./something_triangle_renderer.cpp"
+#include "./something_circle_renderer.cpp"
 #include "./something_camera.cpp"
 #include "./something_main.cpp"
 #include "./something_geo.cpp"
---
+++ src/something_circle_renderer.cpp
@@ -0,0 +1,22 @@
+#include "./something_circle_renderer.hpp"
+
+void Circle_Renderer::init()
+{
+    todo("Circle_Renderer::init()");
+}
+
+void Circle_Renderer::fill_circle(V2<GLfloat> center, GLfloat radius, RGBA color)
+{
+    todo("Circle_Renderer::fill_circle()");
+}
+
+void Circle_Renderer::draw()
+{
+    todo("Circle_Renderer::draw()");
+}
+
+void Circle_Renderer::clear()
+{
+    todo("Circle_Renderer::clear()");
+}
+
