d9b3fa56be6fc9bae55bae15961a40c1ffc88868
zhiayang <zhiayang@nascentnoodle.com>
2021-04-25 23:34:44 +0800

    Update build script to work on macOS

--- build.sh
+++ build.sh
@@ -6,4 +6,8 @@ CXX="${CXX:-g++}"
 PKGS="sdl2 glew"
 CXXFLAGS="-Wall -Wextra -std=c++17 -pedantic -fno-exceptions -ggdb"
 
+if [ `uname` == "Darwin" ]; then
+	CXXFLAGS+=" -framework OpenGL"
+fi
+
 $CXX $CXXFLAGS `pkg-config --cflags $PKGS` -o something.debug src/something.cpp `pkg-config --libs $PKGS`
