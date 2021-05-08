025c0555fc535891cbe8fcbde6522f50f7b3f63a
Danil Kolumbet <kolumbetko@gmail.com>
2021-05-09 02:44:52 +0300

    (#74) Add MSVC release build to CI

--- build_msvc.bat
+++ build_msvc.bat
@@ -1,7 +1,7 @@
 @echo off
 rem launch this from msvs-enabled console
 
-set CXXFLAGS=/std:c++17 /O2 /FC /Zi /W4 /WX /wd4458 /wd4996 /nologo
+set CXXFLAGS=/std:c++17 /FC /Zi /W4 /WX /wd4458 /wd4996 /nologo
 set INCLUDES=/I dependencies\SDL2\include /I dependencies\GLFW\include /I dependencies\GLEW\include
 set LIBS=dependencies\SDL2\lib\x64\SDL2.lib ^
          dependencies\SDL2\lib\x64\SDL2main.lib ^
@@ -9,10 +9,24 @@ set LIBS=dependencies\SDL2\lib\x64\SDL2.lib ^
          dependencies\GLEW\lib\glew32s.lib ^
          opengl32.lib User32.lib Gdi32.lib Shell32.lib
 
+for %%i in (%*) do (
+    if "%%i" == "--release" (
+        set SOMETHING_RELEASE=1
+    ) else (
+        echo ERROR: Unknown flag "%%i"
+        goto :eof
+    )
+)
+
 cl.exe %CXXFLAGS% /Fe"config_indexer.exe" src/config_indexer.cpp
-config_indexer assets/vars.conf >src/config_index.hpp
 
-cl.exe %CXXFLAGS% %INCLUDES% /Fe"something.debug.exe" src/something.cpp ^
-    /link %LIBS% -SUBSYSTEM:windows
+if defined SOMETHING_RELEASE (
+    config_indexer --bake assets/vars.conf >src/config_index.hpp
+    cl.exe /O2 %CXXFLAGS% %INCLUDES% /Fe"something.release.exe" /DSOMETHING_RELEASE src/something.cpp /link %LIBS% -SUBSYSTEM:windows
+) else (
+    config_indexer assets/vars.conf >src/config_index.hpp
+    cl.exe /Od %CXXFLAGS% %INCLUDES% /Fe"something.debug.exe" src/something.cpp /link %LIBS% -SUBSYSTEM:windows
+)
 
-rem TODO(#74): no release build on CI for msvc
+rem To remove enviroment variable from current cmd session
+set SOMETHING_RELEASE=
